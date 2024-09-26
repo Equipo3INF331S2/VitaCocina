78a95309f7287d92bcd995b58fc77c298b146f49
Hugo <hugo.sepulvedao@usm.cl>
2024-09-26 19:30:17 -0300

    add card with recipe image and tags

--- vitacocina/src/pages/RecipePage.js
+++ vitacocina/src/pages/RecipePage.js
@@ -2,8 +2,14 @@ import * as React from 'react';
 import Typography from '@mui/material/Typography';
 import Rating from '@mui/material/Rating';
 import Box from '@mui/material/Box'
+import Card from '@mui/material/Card'
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
 import Divider from '@mui/material/Divider';
+import Image from '../assets/food.jpg'
 export default function RecipePage() {
+  const tagsTitle = ["Dieta", "Tiempo", "Dificultad"];
+
   const calculateAverageRating = (recipe) => {
     const { reviews } = recipe;
     if (reviews.length === 0) return -1;
@@ -65,6 +71,55 @@ const averageRating = calculateAverageRating(exampleRecipe);
         <Divider orientation='vertical' flexItem sx={{ marginX: 1.5 }}/>
         <Typography href='#reviews' component='a'>{exampleRecipe.reviews.length} Reseñas</Typography>
       </Box>
+
+      <Typography>{exampleRecipe.description}</Typography>
+      <Typography lineHeight={2} variant='body2'>Publicado por <strong>{exampleRecipe.author}</strong></Typography>
+
+      <Card sx={{ maxWidth: 600, margin: '0 auto', boxShadow: 3, borderRadius: 4 }}>
+      <CardMedia component="img" height="320" image={Image} alt="food image"/>
+      <CardContent>
+        <Box sx={{ display: 'flex', gap: 1.5, flexWrap: 'wrap', marginTop: 3, justifyContent: 'center'}}>
+          <Box
+            sx={{
+              backgroundColor: '#81d4fa',
+              padding: '6px 12px',
+              borderRadius: '20px',
+              boxShadow: '0 4px 10px rgba(0,0,0,0.1)',
+              fontSize: '0.9rem',
+              fontWeight: '500',
+            }}
+          >
+            {tagsTitle[0]}: {exampleRecipe.dietaryPreferences}
+          </Box>
+
+          <Box
+            sx={{
+              backgroundColor: '#ffd54f',
+              padding: '6px 12px',
+              borderRadius: '20px',
+              boxShadow: '0 4px 10px rgba(0,0,0,0.1)',
+              fontSize: '0.9rem',
+              fontWeight: '500',
+            }}
+          >
+            {tagsTitle[1]}: {exampleRecipe.time}
+          </Box>
+
+          <Box
+            sx={{
+              backgroundColor: '#a5d6a7',
+              padding: '6px 12px',
+              borderRadius: '20px',
+              boxShadow: '0 4px 10px rgba(0,0,0,0.1)',
+              fontSize: '0.9rem',
+              fontWeight: '500',
+            }}
+          >
+            {tagsTitle[2]}: {exampleRecipe.difficulty}
+          </Box>
+        </Box>
+      </CardContent>
+      </Card>
     </>
   );
 }
