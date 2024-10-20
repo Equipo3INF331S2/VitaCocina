185890b18e0bbcaa27e960a8765a9c1aa65db6bb
KuVe076 <gonzalo.alarconc@usm.cl>
2024-10-20 19:31:47 -0300

    Fixed allrecipe and alltip page

--- vitacocina/src/pages/AllRecipePage.js
+++ vitacocina/src/pages/AllRecipePage.js
@@ -1,29 +1,61 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Grid from '@mui/material/Grid';
 import RecipeCard from '../components/RecipeCard';
 import Link from '@mui/material/Link';
 import Typography from '@mui/material/Typography';
 import { Box } from '@mui/system';
+import CircularProgress from '@mui/material/CircularProgress';
+
+
+const ENDPOINT = process.env.ENPOINT || 'http://localhost:5000';
 
 
 const AllRecipePage = () => {
-    // Ejemplo de datos para las tarjetas de recetas (reemplaza con tus datos reales)
-    const recipeData = Array(30).fill({  // Crea 30 recetas de ejemplo
-        title: 'Nombre de la Receta',
-        image: 'url_de_la_imagen', // Reemplaza con la URL real de la imagen
-        description: 'Descripción breve de la receta.',
-    });
+
+    const [recipe, setRecipe] = useState([]);
+    const [loading, setLoading] = useState([true]);
+
+    useEffect(() => {
+        setLoading(true);
+        fetch(`${ENDPOINT}/api/recipes`, { method: 'GET' })
+            .then(response => response.json())
+            .then(data => {
+                const sortedRecipes = [...data].sort((a, b) => a.name.localeCompare(b.name));
+                setRecipe(sortedRecipes);
+                setLoading(false);
+            })
+            .catch(error => {
+                console.error("Error fetching recipes:", error);
+                setLoading(false);
+            });
+    }, []);
+
+    if (loading) {
+        return (
+            <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
+                <img src="/Logo.png" alt="Vita Cocina Logo" style={{ height: '15rem', width: 'auto', marginBottom: '1rem' }} />
+                <Typography variant="h6" color="textSecondary">Cargando Recetas...</Typography>
+                <CircularProgress sx={{ marginTop: '1rem' }} />
+            </Box>
+        );
+    }
+
+    if (!recipe || recipe.length === 0) {
+        return (
+            <Box sx={{ paddingTop: '100px', textAlign: 'center' }}>
+                <Typography variant="h6">No se encontraron Recetas.</Typography>
+            </Box>
+        );
+    }
 
     return (
         <div>
-
-            
             <Typography
                 variant="h3"
                 component="h3"
                 sx={{
                     fontWeight: 'bold',
-                    color: '#FFFFFF',
+                    color: '#000000',
                     fontSize: 'clamp(1rem, 4vw, 6rem)',
                     textAlign: 'left',
                     marginLeft: '10%'
@@ -32,19 +64,8 @@ const AllRecipePage = () => {
                 Recetas
             </Typography>
 
-
             <Box sx={{ display: 'flex', justifyContent: 'center', width: '100%', mt: '2%' }}>
-                <Grid container spacing={2} id="recipe-grid" sx={{ width: '100%', maxWidth: '1200px', direction: { xs: 'column', sm: 'row' } }}> {/* Contenedor principal */}
-                    {recipeData.map((recipe, index) => (
-                        <Grid item key={recipe.id} xs={12} sm={6} md={4}> {/* Define el ancho de las columnas */}
-                            <RecipeCard 
-                                title={recipe.title}
-                                image={recipe.image}
-                                description={recipe.description}
-                            />
-                        </Grid>
-                    ))}
-                </Grid>
+                <RecipeCard recipeData={recipe} />
             </Box>
         </div>
     );
--- vitacocina/src/pages/AllTipPage.js
+++ vitacocina/src/pages/AllTipPage.js
@@ -1,18 +1,53 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Grid from '@mui/material/Grid';
 import TipCard from '../components/TipCard';
 import Link from '@mui/material/Link';
 import Typography from '@mui/material/Typography';
 import { Box } from '@mui/system';
+import CircularProgress from '@mui/material/CircularProgress';
 
+
+
+const ENDPOINT = process.env.ENPOINT || 'http://localhost:5000';
 
 const AllTipPage = () => {
-    // Ejemplo de datos para las tarjetas de recetas (reemplaza con tus datos reales)
-    const tipData = Array(30).fill({  // Crea 30 recetas de ejemplo
-        title: 'Nombre de la Receta',
-        image: 'url_de_la_imagen', // Reemplaza con la URL real de la imagen
-        description: 'Descripción breve de la receta.',
-    });
+
+    const [tips, setTips] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        setLoading(true);
+        fetch(`${ENDPOINT}/api/tips`, { method: 'GET' })
+            .then(response => response.json())
+            .then(data => {
+                const sortedTips = [...data].sort((a, b) => a.title.localeCompare(b.title));
+                setTips(sortedTips);
+                setLoading(false);
+            })
+            .catch(error => {
+                console.error("Error fetching tips:", error);
+                setLoading(false);
+            });
+    }, []);
+
+
+    if (loading) {
+        return (
+            <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh' }}> 
+                <img src="/Logo.png" alt="Vita Cocina Logo" style={{ height: '15rem', width: 'auto', marginBottom: '1rem' }} />
+                <Typography variant="h6" color="textSecondary">Cargando Consejos...</Typography> 
+                <CircularProgress sx={{marginTop:'1rem'}}/>
+            </Box>
+        );
+    }
+
+    if (!tips || tips.length === 0) {
+        return (
+            <Box sx={{ paddingTop: '100px', textAlign: 'center' }}>
+                <Typography variant="h6">No se encontraron Consejos.</Typography>
+            </Box>
+        );
+    }
 
     return (
         <div>
@@ -30,18 +65,7 @@ const AllTipPage = () => {
                 Consejos
             </Typography>
             <Box sx={{ display: 'flex', justifyContent: 'center', width: '100%', mt: '2%' }}>
-                <Grid container spacing={2} id="recipe-grid" sx={{ width: '100%', maxWidth: '1200px', direction: { xs: 'column', sm: 'row' } }}> {/* Contenedor principal */}
-                    {tipData.map((tip, index) => (
-                        <Grid item key={tip.id} xs={12} sm={6} md={4}> {/* Define el ancho de las columnas */}
-                            <TipCard
-                                title={tip.title}
-                                image={tip.image}
-                                description={tip.description}
-                                author={tip.author}
-                            />
-                        </Grid>
-                    ))}
-                </Grid>
+                <TipCard tipData={tips} /> {/* Pasa todos los tips a TipCard */}
             </Box>
         </div>
     );
