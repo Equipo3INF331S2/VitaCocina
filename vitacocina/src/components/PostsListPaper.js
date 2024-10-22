c7946e8e72f27396c0e32a566e14ad6e1301e915
Hugo <hugo.sepulvedao@usm.cl>
2024-10-21 22:46:07 -0300

    connect posts page to endpoints + improve design

--- vitacocina/src/components/PostsListPaper.js
+++ vitacocina/src/components/PostsListPaper.js
@@ -1,42 +1,106 @@
 import React from 'react';
-
+import { useNavigate } from 'react-router-dom';
 import { Paper, List, ListItem, Typography, Box, IconButton } from '@mui/material';
-
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
 
+const ENDPOINT = process.env.ENPOINT || 'http://localhost:5000';
+
 const PostListPaper = ({ posts, type, handleEdit, handleDelete, fallbackText }) => {
+  const navigate = useNavigate();
+
+  const handlePaperClick = (postId) => {
+    if (type === 'recipe') {
+      navigate(`/recipes/${postId}`);
+    } else if (type === 'tip') {
+      navigate(`/tips/${postId}`);
+    }
+  };
 
   return (
     <Paper elevation={4} sx={{ p: 3, height: '480px', overflowY: 'auto' }}>
       {posts.length === 0 ? (
-        <Box sx={{ height: '100%', display: 'flex', alignItems: 'center'}}>
+        <Box sx={{ height: '100%', display: 'flex', alignItems: 'center' }}>
           <Typography variant='h3' component='h6' textAlign='center'>{fallbackText}</Typography>
         </Box>
       ) : (
-      <List>
-        {posts.map((post) => (
-          <Paper variant='outlined' key={post.id} sx={{ mb: 2, p: 2 }}>
-            <ListItem sx={{ display: 'flex', justifyContent: 'space-between' }}>
-              <Typography variant="h6">{post.title}</Typography>
-              <Box>
-                <IconButton
-                  color="primary"
-                  onClick={() => handleEdit(type, post.id)}
-                >
-                  <EditIcon />
-                </IconButton>
-                <IconButton
-                  color="error"
-                  onClick={() => handleDelete(type, post.id)}
-                >
-                  <DeleteIcon />
-                </IconButton>
-              </Box>
-            </ListItem>
-          </Paper>
-        ))}
-      </List>
+        <List>
+          {posts.map((post) => (
+            <Paper 
+              variant='outlined' 
+              key={post._id} 
+              sx={{
+                mb: 2,
+                p: 2,
+                backgroundImage: `url(${ENDPOINT}/uploads/${post.img})`,
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+                color: 'white',
+                position: 'relative',
+                cursor: 'pointer' // Cambia el cursor para indicar que es clickeable
+              }}
+              onClick={() => handlePaperClick(post._id)}
+            >
+              <ListItem sx={{ display: 'flex', justifyContent: 'space-between', position: 'relative', zIndex: 1 }}>
+                <Typography variant="h6">{type === 'recipe' ? post.name : post.title}</Typography>
+                <Box sx={{ zIndex: 1, display: 'flex', gap: 1 }}>
+                  <Box
+                    sx={{
+                      backgroundColor: 'white',
+                      borderRadius: '50%',
+                      padding: 0.25,
+                      display: 'flex',
+                      alignItems: 'center',
+                      justifyContent: 'center',
+                      transition: 'background-color 0.3s',
+                      '&:hover': { backgroundColor: 'lightblue' }
+                    }}
+                  >
+                    <IconButton
+                      color="primary"
+                      onClick={() => handleEdit(type, post._id)}
+                    >
+                      <EditIcon />
+                    </IconButton>
+                  </Box>
+                  <Box
+                    sx={{
+                      backgroundColor: 'white',
+                      borderRadius: '50%',
+                      padding: 0.25,
+                      display: 'flex',
+                      alignItems: 'center',
+                      justifyContent: 'center',
+                      transition: 'background-color 0.3s',
+                      '&:hover': { backgroundColor: 'lightblue' }
+                    }}
+                  >
+                    <IconButton
+                      color="error"
+                      onClick={(e) => {
+                        e.stopPropagation(); // Evita que el clic se propague al Paper
+                        handleDelete(type, post._id);
+                      }}
+                    >
+                      <DeleteIcon />
+                    </IconButton>
+                  </Box>
+                </Box>
+              </ListItem>
+              <Box 
+                sx={{
+                  position: 'absolute',
+                  top: 0,
+                  left: 0,
+                  right: 0,
+                  bottom: 0,
+                  backgroundColor: 'rgba(0, 0, 0, 0.55)',
+                  zIndex: 0
+                }}
+              />
+            </Paper>
+          ))}
+        </List>
       )}
     </Paper>
   );
--- vitacocina/src/pages/MyPostsPage.js
+++ vitacocina/src/pages/MyPostsPage.js
@@ -1,48 +1,80 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Typography, Container, Button, Box } from '@mui/material';
 import Grid from '@mui/material/Grid2';
 
 import PostListPaper from '../components/PostsListPaper';
 
-// Ejemplos de recetas y consejos
-const exampleRecipes = [
-  { id: 1, title: "Ensalada César" },
-  { id: 2, title: "Tarta de Manzana" },
-  { id: 3, title: "Queque de vainilla" },
-  { id: 4, title: "Galletas de chocolate" },
-  { id: 5, title: "Mermelada de frutilla" },
-  { id: 6, title: "Tortilla de papas" },
-  { id: 7, title: "Albóndigas" },
-  
-];
-
-const exampleTips = [
-  { id: 1, title: "Consejo para asar carnes" },
-  { id: 2, title: "Cómo hacer una vinagreta perfecta" },
-];
+const ENDPOINT = process.env.ENPOINT || 'http://localhost:5000';
 
 const MyPostsPage = () => {
   const navigate = useNavigate();
-  const [recipes, setRecipes] = useState(exampleRecipes);
-  const [tips, setTips] = useState(exampleTips);
+  const user = JSON.parse(localStorage.getItem('user'));
+  const [recipes, setRecipes] = useState([]);
+  const [tips, setTips] = useState([]);
 
   const fallbackText = {
     recipes: 'No tienes ningúna receta publicada.',
     tips: 'No tienes ningún consejo publicado.'
   }
 
+  useEffect(() => {
+    fetch(`${ENDPOINT}/api/recipes/user/${user._id}`, { method: 'GET' })
+        .then(response => response.json())
+        .then(data => {
+          setRecipes(data);
+        })
+        .catch(error => {
+            console.error("Error obteniendo recetas:", error);
+        });
+  }, [user._id]);
+
+  useEffect(() => {
+    fetch(`${ENDPOINT}/api/tips/user/${user._id}`, { method: 'GET' })
+        .then(response => response.json())
+        .then(data => {
+          setTips(data);
+        })
+        .catch(error => {
+            console.error("Error obteniendo consejos:", error);
+        });
+  }, [user._id]);
+
   const handleEdit = (type, id) => {
     console.log(`Edit ${type} with ID: ${id}`);
   };
 
-  const handleDelete = (type, id) => {
-    if (type === 'recipe') {
-      setRecipes((prev) => prev.filter((recipe) => recipe.id !== id));
-    } else if (type === 'tip') {
-      setTips((prev) => prev.filter((tip) => tip.id !== id));
+  const handleDelete = async (type, id) => {
+    try {
+      let endpoint;
+
+      switch (type) {
+        case 'recipe':
+          endpoint = `${ENDPOINT}/api/recipe/${id}`;
+          break;
+        case 'tip':
+          endpoint = `${ENDPOINT}/api/tips/${id}`;
+          break;
+        default:
+          throw new Error('Tipo no válido');
+      }
+
+      const response = await fetch(endpoint, { method: 'DELETE' });
+  
+      if (!response.ok) {
+        const errorData = await response.json();
+        throw new Error(errorData.message || 'Error al eliminar');
+      }
+
+      if (type === 'recipe') {
+        setRecipes((prev) => prev.filter((recipe) => recipe._id !== id));
+      } else if (type === 'tip') {
+        setTips((prev) => prev.filter((tip) => tip._id !== id));
+      }
+    } catch (error) {
+      console.error('Error al eliminar:', error.message);
+      alert('Ocurrió un error al eliminar: ' + error.message);
     }
-    console.log(`Delete ${type} with ID: ${id}`);
   };
 
   return (
