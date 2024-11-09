0ffc65fe24923b27df4a782b9583e7114e6136f1
Hugo <hugo.sepulvedao@usm.cl>
2024-11-08 21:14:16 -0300

    tiny fix

--- server.js
+++ server.js
@@ -13,10 +13,7 @@ app.use(cors());
 app.use('/uploads', express.static('uploads'));
 
 // Conectar a MongoDB
-  mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }).then(() => console.log('Connected to MongoDB'))
+  mongoose.connect(process.env.MONGO_URI).then(() => console.log('Connected to MongoDB'))
     .catch((err) => console.error('Could not connect to MongoDB', err));
 
 // Rutas
