63e3e4925fef3684650084a9c8037fb388aac9ae
KuVe076 <gonzalo.alarconc@usm.cl>
2024-11-26 11:48:35 -0300

    Add test to enter alltips page

---
+++ vitacocina/test/EnterAllTipsPage.spec.js
@@ -0,0 +1,30 @@
+const { Builder, By, until } = require('selenium-webdriver');
+
+async function clickTipsTitleTest() {
+  let driver = await new Builder().forBrowser('chrome').build();
+
+  try {
+    await driver.get('http://localhost:3000/');
+
+    await driver.wait(until.elementLocated(By.linkText('Consejos')), 10000);
+    const tipsLink = await driver.findElement(By.linkText('Consejos'));
+    await tipsLink.click();
+
+    await driver.wait(until.urlContains('/alltip'), 10000);
+
+    const currentUrl = await driver.getCurrentUrl();
+    if (!currentUrl.includes('/alltip')) {
+      throw new Error('No se navegó a la página "All Tips" correctamente');
+    }
+
+    console.log('clickTipsTitleTest Exitoso');
+
+  } catch (err) {
+    console.error(`Error durante la prueba: ${err}`);
+
+  } finally {
+    await driver.quit();
+  }
+}
+
+clickTipsTitleTest();
