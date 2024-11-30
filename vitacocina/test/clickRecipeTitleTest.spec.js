c2ca44a72a23a3dedba57f4a6f7349e13113a55b
Hugo <hugo.sepulvedao@usm.cl>
2024-11-30 00:07:32 -0300

    minor change to test jenkins

--- vitacocina/test/clickRecipeTitleTest.spec.js
+++ vitacocina/test/clickRecipeTitleTest.spec.js
@@ -14,7 +14,7 @@ async function clickRecipeTitleTest() {
   .build();
 
   try {
-    await driver.get('http://20.201.119.103:3000/');
+    await driver.get('http://localhost:3000/');
 
     await driver.wait(until.elementLocated(By.linkText('Recetas')), 10000);
     const recipeLink = await driver.findElement(By.linkText('Recetas'));
